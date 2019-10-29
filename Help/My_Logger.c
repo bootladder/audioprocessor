72768eee8448358ec0dda517131f1d199369e728
Steve Anderson <steve@bootladder.com>
2019-10-29 10:15:25 -0400

    My_Logger change args to char const * str
    do aid in C++ literal string use

--- Help/My_Logger.c
+++ Help/My_Logger.c
@@ -7,13 +7,13 @@ static char * log_ptr = log_buffer;
 
 
 
-void My_Logger_LogString(char * str)
+void My_Logger_LogString(char * const str)
 {
   int bytesWritten = tfp_snprintf(log_ptr, BUFFER_SIZE, "%s", str);
   log_ptr += bytesWritten;
 }
 
-void My_Logger_LogStringLn(char * str)
+void My_Logger_LogStringLn(char * const str)
 {
   //don't allow writing past the buffer
 
@@ -24,7 +24,7 @@ void My_Logger_LogStringLn(char * str)
   log_ptr += bytesWritten;
 }
 
-void My_Logger_LogFilenameAndLine(char * str, int line)
+void My_Logger_LogFilenameAndLine(char * const str, int line)
 {
   //don't allow writing past the buffer
 
@@ -40,7 +40,7 @@ void My_Logger_LogFilenameAndLine(char * str, int line)
 
 char * logger_oneshotstrings[100];
 
-void My_Logger_SetOneTime(MY_LOGGER_ONESHOTS index, char * str)
+void My_Logger_SetOneTime(MY_LOGGER_ONESHOTS index, char * const str)
 {
   logger_oneshotstrings[index] = str;
 }
--- Help/My_Logger.h
+++ Help/My_Logger.h
@@ -1,9 +1,9 @@
 
-void My_Logger_LogString(char * str);
-void My_Logger_LogStringLn(char * str);
+void My_Logger_LogString(char * const str);
+void My_Logger_LogStringLn(char * const str);
 void My_Logger_LogLineNumber(int line);
-void My_Logger_LogStringSpace(char * str);
-void My_Logger_LogFilenameAndLine(char * str, int line);
+void My_Logger_LogStringSpace(char * const str);
+void My_Logger_LogFilenameAndLine(char * const str, int line);
 
 #define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
 #define RUN_AND_LOG(a) do{ My_Logger_LogString("\n"); My_Logger_LogFilenameAndLine( __FILENAME__ , __LINE__ ); My_Logger_LogStringLn(#a); a;} while(0);
@@ -17,7 +17,7 @@ typedef enum {
 } MY_LOGGER_ONESHOTS;
 
 
-void My_Logger_SetOneTime(MY_LOGGER_ONESHOTS index, char * str);
+void My_Logger_SetOneTime(MY_LOGGER_ONESHOTS index, char * const str);
 
 #define LOG_ONESHOT(s) \
   {static short happened = 0;                  \
