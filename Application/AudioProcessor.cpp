72978612d02f7664b704e7b58f24de04f7e3c5d3
Steve Anderson <steve@bootladder.com>
2019-10-29 15:35:26 -0400

    AudioProcessor split up, C++ linkage and C linkage
    Now there's a clean separation between
    C++ and C.  The AudioProcess_Process is C linkage,
    everything behind it is C++

--- Application/AudioProcessor.cpp
+++ Application/AudioProcessor.cpp
@@ -1,15 +1,22 @@
 extern "C" {
-
 #include "AudioProcessor.h"
+}
 
 static int16_t dummyBuf[1024];
+static int16_t add_something(uint32_t i);
 
-int16_t * AudioProcessor_ProcessSampleBuffer(int16_t * sampleBuf, uint32_t num_samples)
+
+extern "C"
+int16_t *
+AudioProcessor_ProcessSampleBuffer(int16_t * sampleBuf, uint32_t num_samples)
 {
   for(uint32_t i=0; i < num_samples; i++){
-    dummyBuf[i] = sampleBuf[i];
+    dummyBuf[i] = sampleBuf[i] + add_something(num_samples);
   }
   return dummyBuf;
 }
 
+static int16_t add_something(uint32_t i)
+{
+  return 0;
 }
