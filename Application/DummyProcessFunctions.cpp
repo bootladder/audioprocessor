4c10eb0446faaed29f0cfdbd99b11988f9efcd06
Steve Anderson <steve@bootladder.com>
2019-10-30 22:53:58 -0400

    DummyProcessFunctions add noop and add_2

--- Application/DummyProcessFunctions.cpp
+++ Application/DummyProcessFunctions.cpp
@@ -1,5 +1,11 @@
 #include "DummyProcessFunctions.hpp"
 
+int16_t * DummyProcessFunctions_noop(int16_t * buf, uint32_t num_samples)
+{
+  (void)num_samples;
+  return buf;
+}
+
 int16_t * DummyProcessFunctions_half_volume(int16_t * buf, uint32_t num_samples)
 {
   for(uint32_t i=0; i<num_samples; i++){
@@ -12,6 +18,14 @@ int16_t * DummyProcessFunctions_add_1(int16_t * buf, uint32_t num_samples)
 {
   for(uint32_t i=0; i<num_samples; i++){
     buf[i] = buf[i] + 1;
+  }
+  return buf;
+}
+
+int16_t * DummyProcessFunctions_add_2(int16_t * buf, uint32_t num_samples)
+{
+  for(uint32_t i=0; i<num_samples; i++){
+    buf[i] = buf[i] + 2;
   }
   return buf;
 }
--- Application/DummyProcessFunctions.hpp
+++ Application/DummyProcessFunctions.hpp
@@ -1,4 +1,6 @@
 #include <stdint.h>
 
+int16_t * DummyProcessFunctions_noop(int16_t * buf, uint32_t num_samples);
 int16_t * DummyProcessFunctions_half_volume(int16_t * buf, uint32_t num_samples);
 int16_t * DummyProcessFunctions_add_1(int16_t * buf, uint32_t num_samples);
+int16_t * DummyProcessFunctions_add_2(int16_t * buf, uint32_t num_samples);
