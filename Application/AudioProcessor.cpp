dc0ef5c9cd5d5cfe60a6879ecf57712a785407d0
Steve Anderson <steve@bootladder.com>
2019-11-04 23:57:30 -0500

    class AudioProcessor with init() and process()
    using the num_samples from the call to ProcessSampleBuffer.
    not sure how to support runtime num_samples.
    Need to initalize buffer sizes at compile time.
    init() is a stub, must be called by the task init

--- Application/AudioProcessor.cpp
+++ Application/AudioProcessor.cpp
@@ -9,28 +9,43 @@ extern "C" {
 #include "ProcessBlockFunctions_FIRFilters.hpp"
 #include "FIRBlock.hpp"
 
-static sample_t inputSampleBuffer[MY_PROCESSING_BUFFER_SIZE_SAMPLES];
-static int16_t outputInt16Buffer[MY_PROCESSING_BUFFER_SIZE_SAMPLES];
+class AudioProcessor{
+public:
+  void init(void);
+  sample_t * process(sample_t * sampleBuf);
+};
 
-static sample_t * __testing__process_sample_buffer(sample_t * sampleBuf);
+AudioProcessor audioProcessor;
 
+// This is called by the FreeRTOS Audio Task, every time the DMA receive buf fills up.
+// Takes int16_t samples, processes, returns int16_t samples.
+// The processor takes sample_t's, currently defined to be floats
+
+// The AudioProcessor MUST be initialized!!
+// Make a call to AudioProcessor_Init() !!!
 extern "C"
 int16_t *
 AudioProcessor_ProcessSampleBuffer(int16_t * sampleBuf, uint32_t num_samples)
 {
-  (void)num_samples;
+  static sample_t inputSampleBuffer[MY_PROCESSING_BUFFER_SIZE_SAMPLES];  //max size
+  static int16_t outputInt16Buffer[MY_PROCESSING_BUFFER_SIZE_SAMPLES];
 
-  for(uint32_t i=0; i<MY_PROCESSING_BUFFER_SIZE_SAMPLES; i++){
+  for(uint32_t i=0; i<num_samples; i++){
     inputSampleBuffer[i] = (sample_t)sampleBuf[i];
   }
 
-  sample_t * out = __testing__process_sample_buffer(inputSampleBuffer);
+  sample_t * out = audioProcessor.process(inputSampleBuffer);
 
-
-  for(uint32_t i=0; i<MY_PROCESSING_BUFFER_SIZE_SAMPLES; i++){
+  for(uint32_t i=0; i<num_samples; i++){
     outputInt16Buffer[i] = (int16_t)out[i];
   }
+
   return outputInt16Buffer;
+}
+
+extern "C" void AudioProcessor_Init(void)
+{
+  audioProcessor.init();
 }
 
 
@@ -42,25 +57,28 @@ static GainBlock gainBlock1(MY_PROCESSING_BUFFER_SIZE_SAMPLES);
 static GainBlock gainBlock2(MY_PROCESSING_BUFFER_SIZE_SAMPLES);
 
 static ClippingDistortionBlock clippingBlock1(MY_PROCESSING_BUFFER_SIZE_SAMPLES);
-//ProcessBlockFunctions_ClippingDistortion, 
 
-static sample_t * __testing__process_sample_buffer(sample_t * sampleBuf)
+void AudioProcessor::init(void)
+{
+}
+
+sample_t * AudioProcessor::process(sample_t * sampleBuf)
 {
   sample_t * out;
 
-    gainBlock1.setParam(PARAM_0, 4);  //don't set the param every time
-    gainBlock1.process(sampleBuf);
-    out = gainBlock1.getOutputBuffer();
-  //
-    clippingBlock1.process(out);
-    out = clippingBlock1.getOutputBuffer();
-  //
+  gainBlock1.setParam(PARAM_0, 4);  //don't set the param every time
+  gainBlock1.process(sampleBuf);
+  out = gainBlock1.getOutputBuffer();
+
+  clippingBlock1.process(out);
+  out = clippingBlock1.getOutputBuffer();
+
   //  gainBlock2.setParam(PARAM_0, 2);  //don't set the param every time
   //  gainBlock2.process(out);
   //  out = gainBlock2.getOutputBuffer();
-  //
-    firBlock1.process(out);
-    out = firBlock1.getOutputBuffer();
+
+  firBlock1.process(out);
+  out = firBlock1.getOutputBuffer();
 
   firBlock2.process(out);
   out = firBlock2.getOutputBuffer();
