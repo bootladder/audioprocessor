6345bc577a30171ed03f670cc07b1b82393c5d10
Steve Anderson <steve@bootladder.com>
2019-11-13 14:41:12 -0500

    Cleanup: tinyprintf no override libc, fake seriallog prints
    and add include guard aroudn MIDIMessageHanderl.hpp

--- Application/MIDIMessageHandler.hpp
+++ Application/MIDIMessageHandler.hpp
@@ -1,2 +1,7 @@
+#ifndef __MIDIMESSAGEHANDLER_HPP__
+#define __MIDIMESSAGEHANDLER_HPP__
+
 #include "MIDIMap.hpp"
 void MIDIMessageHandler_RegisterMIDIMap(MIDIMap & map);
+
+#endif
--- test/FAKE_SerialLogger.c
+++ test/FAKE_SerialLogger.c
@@ -1,9 +1,11 @@
 #include "SerialLogger.h"
 
 #include <stdint.h>
+#include <stdio.h>
 void SerialLogger_Log(LogType_t type, uint8_t * str, uint32_t size)
 {
   (void)type;
   (void)str;
   (void)size;
+  fwrite(str, sizeof(uint8_t), size, stdout);
 }
