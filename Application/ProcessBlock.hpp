4f367621ecb3c1e532ec13ef05dabd7f988be309
Steve Anderson <steve@bootladder.com>
2019-11-15 20:09:16 -0500

    Mixer prevents overloading by dividing by max value

--- Application/ProcessBlock.hpp
+++ Application/ProcessBlock.hpp
@@ -203,8 +203,27 @@ public:
       inputBuffer[i] = samplesToProcess[i];
     }
 
+
+    //track max
+    sample_t max = 0;
+    sample_t min = 0;
+    
     for(uint32_t i=0; i<num_samples; i++){
       outputBuffer[i] = outputBuffer[i] + inputBuffer[i];
+      if(outputBuffer[i] > max)
+        max = outputBuffer[i];
+      if(outputBuffer[i] < min)
+        min = outputBuffer[i];
+    }
+
+    min = min*-1;
+    sample_t absmax = max > min ? max : min;
+
+    if(absmax > 32700){
+      SerialLogger_LogLiteralString(LOGTYPE_EVENT, "MIXER OVERLOAD\n");
+      for(uint32_t i=0; i<num_samples; i++){
+        outputBuffer[i] = outputBuffer[i] * (32700/absmax);
+      }
     }
   }
 };
