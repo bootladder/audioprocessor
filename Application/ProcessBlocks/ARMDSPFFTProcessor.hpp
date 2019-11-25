4a40f91a5f3600ad05499c0218e46c123b15c69a
Steve Anderson <steve@bootladder.com>
2019-11-25 16:13:05 -0500

    Move files under ProcessBlocks/
    Reduces clutter, as I am about to add FIR subclasses

--- Application/ProcessBlocks/ARMDSPFFTProcessor.hpp
+++ Application/ProcessBlocks/ARMDSPFFTProcessor.hpp
@@ -17,9 +17,9 @@ private:
 public:
   sample_t * calculateFFT(sample_t * fft_buf, sample_t * resultSpectrum, uint32_t size)
   {
-    RUN_AND_LOG(arm_rfft_fast_init_f32(&S, size););
-    RUN_AND_LOG(arm_rfft_fast_f32(&S, fft_buf, resultSpectrum, 0); ); //0 for fft, 1 for ifft
-    RUN_AND_LOG(arm_cmplx_mag_f32(resultSpectrum, resultSpectrum, size/2);  );
+    arm_rfft_fast_init_f32(&S, size);
+    arm_rfft_fast_f32(&S, fft_buf, resultSpectrum, 0);  //0 for fft, 1 for ifft
+    arm_cmplx_mag_f32(resultSpectrum, resultSpectrum, size/2);
 
     return fft_buf;
   }
