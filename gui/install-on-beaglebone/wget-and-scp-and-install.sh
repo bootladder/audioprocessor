186b041d22b0ec7e1ff3217bc32811cf06eb8553
Steve Anderson <steve@bootladder.com>
2020-02-04 19:07:29 -0500

    parameterize gui wget-and-scp-and-install.sh

--- gui/install-on-beaglebone/wget-and-scp-and-install.sh
+++ gui/install-on-beaglebone/wget-and-scp-and-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+DESTINATION_ON_TARGET=/mnt/packages/graphviz
+
 file=$(wget $1 2>&1 | grep Saving | cut -d ' ' -f 3 | sed -e 's/[^A-Za-z0-9.\+_-]//g')
-scp $file beaglebone-default:/mnt/packages/python-tk
-ssh -t beaglebone-default sudo dpkg -i /mnt/packages/python-tk/$file
+scp $file beaglebone-default:$DESTINATION_ON_TARGET
+ssh -t beaglebone-default sudo dpkg -i $DESTINATION_ON_TARGET/$file
 rm $file
 
