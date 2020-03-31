be693ca1c60d18c6bb0982852cee3a0a6ce9eca9
Steve Anderson <steve@bootladder.com>
2020-03-30 20:53:13 -0400

    server simply forwards all recieved lines on serial to the socket

--- gui/backendserver/backendserver.py
+++ gui/backendserver/backendserver.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python
 import os
 import sys
-
+import socket
 import thread
 import time
 import serial
-import subprocess
-import json
-import select
+
+server_address = '/tmp/mysocket'
+
+# Make sure the socket does not already exist
+try:
+    os.unlink(server_address)
+except OSError:
+    if os.path.exists(server_address):
+        raise
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+print >>sys.stderr, 'starting up on %s' % server_address
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+changingstate = 0
+
+# global variable for connection
+connection = 0
 
 
-
-
-def thread_seriallogger_reader(dispatch, arg2, arg3):
+# Forward all received lines to the socket client
+def thread_seriallogger_reader(arg):
 
     with serial.Serial('/dev/ttyUSB0', 921600, timeout=2) as ser:
         while(True):
@@ -20,20 +39,35 @@ def thread_seriallogger_reader(dispatch, arg2, arg3):
             if len(line) == 0:
                 continue
 
-            if line[0] in dispatch.keys():
-                dispatch[line[0]](line[1:]) # call it
+            try:
+                connection.sendall(line)
+            except:
+                print 'no client.  sleeping a bit'
+                time.sleep(1)
+                continue
 
 
-###################### MAIN #########################
+thread.start_new_thread(thread_seriallogger_reader, ('arrrr',))
 
-# Serial Port Dispatch
-messageDispatch = {
-    'I':app.updateIdleTickCount,
-    'L':app.updateEventLog,
-    'M':app.updateMidiProcessedLog,
-    'B':app.receivedNodeUpdate,
-    'E':app.receivedEdgeListUpdate,
-}
 
-thread.start_new_thread(thread_seriallogger_reader, (messageDispatch, 'hurr', 'durr'))
+
+# Accept a connection and do nothing
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        while True:
+            try:
+                connection.sendall("DATA FROM SERVER %d" % changingstate)
+            except :
+                break
+            changingstate = changingstate + 1
+            time.sleep(1)
+
+    finally:
+        # Clean up the connection
+        connection.close()
 
