71ea9a50c42dd673fe14363f78b79ed8506e16e1
Steve Anderson <steve@bootladder.com>
2020-05-21 19:31:52 -0400

    MIDIReceiver.cpp experiment to remove unnecessary code sections.
    With --no-execptions and moving member function definitions into the cpp file,
    classes that use MIDIReceiver.hpp now don't have to have the member functions in their compilation units.
    And also in MIDIReceiver.cpp there are no extra sections for those member functions

--- Application/MIDIReceiver.hpp
+++ Application/MIDIReceiver.hpp
@@ -27,27 +27,11 @@ private:
     int midiAssignmentIndex = 0;
 
 public:
-    void assignMIDIMessageToParameter(MIDI_Message_t msg, BlockParamIdentifier_t id){
-        midiAssignments[midiAssignmentIndex].msg = msg;
-        midiAssignments[midiAssignmentIndex].paramId = id;
-        midiAssignmentIndex++;
-    }
+    void assignMIDIMessageToParameter(MIDI_Message_t msg, BlockParamIdentifier_t id);
 
-    virtual void MIDIMessageReceived(MIDI_Message_t &msg){
-        for(int i=0; i<midiAssignmentIndex; i++) {
-            if (midiAssignments[i].msg.type != msg.type)
-                continue;
-            if (midiAssignments[i].msg.id != msg.id)
-                continue;
+    virtual void MIDIMessageReceived(MIDI_Message_t &msg);
 
-            setMIDIParameter(midiAssignments[i].paramId, msg.value);
-        }
-    }
-
-
-    virtual void setMIDIParameter(BlockParamIdentifier_t id, int value){
-        (void)id;(void)value;
-    }
+    virtual void setMIDIParameter(BlockParamIdentifier_t id, int value);
 
 
 };
