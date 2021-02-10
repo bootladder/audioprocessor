8b6d316eaabd85e0ffad725e4dacac6ac33447a1
Steve Anderson <steve@bootladder.com>
2021-02-10 18:23:01 -0500

    more stubs

--- API/WeakAppStubs.c
+++ API/WeakAppStubs.c
@@ -2,6 +2,7 @@
 // Created by steve on 5/21/20.
 //
 #include <stdint.h>
+#include "MIDI_Message.hpp"
 
 #define WEAK __attribute__((weak))
 WEAK void AudioProcessor_Init(void){
@@ -9,5 +10,9 @@ WEAK void AudioProcessor_Init(void){
 }
 WEAK int16_t * AudioProcessor_ProcessSampleBuffer(int16_t * sampleBuf, uint32_t num_samples)
 {
-  return (int16_t *) 0;
+  return sampleBuf;
+}
+
+WEAK void MIDIMessageHandler_Handle(MIDI_Message_t message){
+
 }
